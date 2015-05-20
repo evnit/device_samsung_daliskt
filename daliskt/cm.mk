4bb1d3331eac7966c669043692e5da064a2fa43a
Le Anh Dung <evnithn@gmail.com>
2015-05-20 11:25:57 +0700

    Update cm.mk

--- daliskt/cm.mk
+++ daliskt/cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120K TARGET_DEVICE=SHV-E120K BUILD_FINGERPRINT="samsung/SHV-E120K/SHV-E120K:4.4.1/KOT49E/KKMK1:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120K-user 4.4.1 KOT49E KKMK1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120S TARGET_DEVICE=SHV-E120S BUILD_FINGERPRINT="samsung/SHV-E120S/SHV-E120S:4.4.1/KOT49E/KSJMA4:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120S-user 4.4.1 KOT49E KSJMA4 release-keys"
 
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
